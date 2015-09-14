f46a19fb024bf31d3201c8cda8f52fa964b41c01
Lasse-Fisker <201205998@iha.dk>
2015-09-14 10:34:46 +0200

    Initial project committed
    first project with styles for view

---
+++ VS15 projekt/SPDS/SPDS/Controllers/AccountController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace SPDS.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult ForgotPass()
+        {
+            return View();
+        }
+
+        public ActionResult CreateAccount()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(Models.User user)
+        {
+            if (ModelState.IsValid)
+            {
+                if (user.IsValid(user.UserName, user.Password))
+                {
+                    FormsAuthentication.SetAuthCookie(user.UserName, user.RememberMe);
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Login Data is incorrect!");
+                }
+
+            }
+            return View(user);
+        }
+    }
+}
---
+++ VS15 projekt/SPDS/SPDS/Models/User.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace SPDS.Models
+{
+    public class User
+    {
+        [Required]
+        [Display(Name = "UserName")]
+        public string UserName { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Display(Name = "Remember me?")]
+        public bool RememberMe { get; set; }
+
+        public bool IsValid(string _username, string _password)
+        {
+            using (var cn = new SqlConnection(@"Data Source = (LocalDB)\MSSQLLocalDB; AttachDbFilename = " +
+                                              @"C:\Users\Fiskr\Dropbox\IHA\4. semester\E15I4PRJ\SPDSMVC\SPDS\SPDS\App_Data\users.mdf" +
+                                              @"; Integrated Security = True"))
+            {
+                string _sql = @"SELECT [Username] FROM [dbo].[System_Users] " +
+                              @"WHERE [Username] = @u AND [Password] = @p";
+                var cmd = new SqlCommand(_sql, cn);
+                cmd.Parameters
+                    .Add(new SqlParameter("@u", SqlDbType.NVarChar))
+                    .Value = _username;
+                cmd.Parameters
+                    .Add(new SqlParameter("@p", SqlDbType.NVarChar))
+                    .Value = _password;
+                cn.Open();
+                var reader = cmd.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    reader.Dispose();
+                    cmd.Dispose();
+                    return true;
+                }
+                else
+                {
+                    reader.Dispose();
+                    cmd.Dispose();
+                    return false;
+                }
+
+            }
+        }
+    }
+}
