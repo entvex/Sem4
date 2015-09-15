cb2ad5428dbb4028063f8e10d5ff2ecbb20bc126
David <entvex@gmail.com>
2015-09-15 21:36:31 +0200

    added all the new database stuff and a example in AboutController.cs

--- VS15 projekt/SPDS/SPDS/Controllers/AboutController.cs
+++ VS15 projekt/SPDS/SPDS/Controllers/AboutController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SPDS.Models;
 
 namespace SPDS.Controllers
 {
@@ -11,6 +12,18 @@ namespace SPDS.Controllers
         // GET: About
         public ActionResult About()
         {
+
+            //TSPDSModelContainer dam;
+            //var bruger = new User();
+            //bruger.Email = "jdksad";
+            //bruger.FirstName = "jdksad";
+            //bruger.LastName = "jdksad";
+            //bruger.Institute = "jdksad";
+            //bruger.Password = "jdksad";
+            //bruger.PermissionPermissionId = 1;
+            //dam.UserSet.Add(bruger);
+
+
             return View();
         }
     }
