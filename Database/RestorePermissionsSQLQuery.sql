3a090e70e7666989c2ff5fc0da4731a56f445863
David <entvex@gmail.com>
2015-09-17 08:27:03 +0200

    added new dev database project and gone back to the old version of the database in the main version.

---
+++ Database/RestorePermissionsSQLQuery.sql
@@ -0,0 +1,10 @@
+USE [E15I4PRJ4Gr2]
+GO
+
+INSERT INTO [dbo].[PermissionSet]
+           ([Description])
+     VALUES
+           ("Submitter","Submitter waiting for approveal","Reviewer","Admin")
+GO
+
+
