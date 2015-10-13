bd22b0615d12a0e9ea08f1affacf76ca49314d42
team15109 <201370209@iha.dk>
2015-10-13 06:39:27 +0300

    Added EncryptPassword.cs to encrypt passwords

---
+++ VS15 projekt/SPDS/SPDS/Models/EncryptPassword.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace encryptPassword
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EncryptPassword("RasmusErEnNoob");
+        }
+
+        public static string EncryptPassword(string pass)
+        {
+            byte[] bytes = Encoding.UTF8.GetBytes(pass);
+            var encrypter = new SHA256Managed();
+            string hashedString = String.Empty;
+            byte[] hash = encrypter.ComputeHash(bytes);
+            foreach (var x in hash)
+            {
+                hashedString += $"{x:x2}";
+            }
+            return hashedString;
+        }
+    }
+}
