e6cbc815a87407c715ea6e978fd7daab9b9e1075
Victor Nielsen <victor.nielsen@hotmail.com>
2015-10-16 09:45:38 +0200

    Added Search Function - Not finished

--- SPDS/SPDS/Controllers/HomeController.cs
+++ SPDS/SPDS/Controllers/HomeController.cs
@@ -3,15 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MSSQLModel;
 
 namespace SPDS.Controllers
 {
     public class HomeController : Controller
     {
+
+        IDALRetrieving db = new MSSQLModelDAL();
+        
         // GET: Home
-        public ActionResult Index()
+        public ActionResult Index(string option, string search)
         {
-            return View();
+            if (option == "Name")
+            {
+                 return View(db.GetAllDatasetsByFirstName(search));
+            }
+            else if (option == "Ion")
+            {
+                var ion = db.GetProjectileByName(search);
+                return View(db.GetAllDatasetsByProjectile(ion));
+            }
+            else
+            {
+                var target = db.GetTargetMaterialByName(search);
+                return View(db.GetAllDatasetsByTargetMaterial(target));
+            }
         }
     }
 }
--- VS15 projekt/SPDS/SPDS/Controllers/HomeController.cs
+++ VS15 projekt/SPDS/SPDS/Controllers/HomeController.cs
@@ -3,15 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SPDS.Models;
 
 namespace SPDS.Controllers
 {
     public class HomeController : Controller
     {
+
+        Dataset db = new Dataset();
+
         // GET: Home
-        public ActionResult Index()
+        public ActionResult Index(string option, string search)
         {
-            return View();
+            if(option == "Name")
+            {
+                //return View(db.theDatabase.Where(x => x.Name == search || search == null).ToList());
+            }
+            else if (option == "Ion")
+            {
+               // return View(db.theDatabase.Where(x => x.Ion == search || search == null).ToList());
+            }
+            else if(option == "Projectile")
+            {
+                // return View(db.theDatabase.Where(x => x.Projectile == search || search == null).ToList());
+            }
+            else
+            {
+                // return View(db.theDatabase.Where(x => x.Mass == search || search == null).ToList());
+            }
+            string o = option;
+            string s = search;
+            return View(o, s);
         }
     }
 }
