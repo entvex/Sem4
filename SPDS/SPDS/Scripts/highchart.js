9b1f2fdfea62f0a7f06d97918d576e12f13a9d56
kaspertorp <kaspertorp@yahoo.dk>
2015-10-28 20:19:28 +0100

    Added The new Graph into the project. create the object in Js with var Graph = new graph()
    then use the methods on the object
    
    .drawGraph(dataarray,'#containername')
    
    .drawGraphDoublePlots(dataarray,dataarray2,'#containername')

---
+++ SPDS/SPDS/Scripts/highchart.js
@@ -0,0 +1,119 @@
+function graph( ){
+
+}
+
+graph.prototype.drawGraph = function(graphdata, containername){
+$(function () {
+    $(containername).highcharts({
+
+        chart: {
+            width: 900,
+            height: 600,
+            spacingRight: 20
+        },
+        title: {
+            text: 'Atom# -> Atom#'
+        },
+        subtitle: {
+            text: '#Comment or something here#'
+        },
+
+        xAxis: {
+            type: 'logarithmic',
+            min: 1,
+            tickInterval: 1,
+            minorTickInterval: 0.1,
+            gridLineWidth: 1,
+             title: {
+                text: 'Length (cm)'
+            }
+        },
+
+        yAxis: {
+            type: 'logarithmic',
+            min: 1,
+            tickInterval: 1,
+            minorTickInterval: 0.1,
+            title: {
+                text: 'Power (kVm)'
+            }
+        },
+        tooltip: {
+            headerFormat: '<b>{series.name}</b><br>',
+            pointFormat: '{point.x:.2f} cm : {point.y:.2f} kVm'
+        },
+        
+
+        legend: {
+            enabled: false
+        },
+
+        series: [{
+            name:'Experimental Data',
+            data: graphdata,
+            type: 'scatter'
+        }]
+
+    });
+});
+}
+
+graph.prototype.drawGraphDoublePlots = function(graphdata,graphdata2, containername){
+$(function () {
+    $(containername).highcharts({
+
+        chart: {
+            width: 900,
+            height: 600,
+            spacingRight: 20
+        },
+        title: {
+            text: 'Atom# -> Atom#'
+        },
+        subtitle: {
+            text: '#Comment or something here#'
+        },
+
+        xAxis: {
+            type: 'logarithmic',
+            min: 1,
+            tickInterval: 1,
+            minorTickInterval: 0.1,
+            gridLineWidth: 1,
+             title: {
+                text: 'Length (cm)'
+            }
+        },
+
+        yAxis: {
+            type: 'logarithmic',
+            min: 1,
+            tickInterval: 1,
+            minorTickInterval: 0.1,
+            title: {
+                text: 'Power (kVm)'
+            }
+        },
+        tooltip: {
+            headerFormat: '<b>{series.name}</b><br>',
+            pointFormat: '{point.x:.2f} cm : {point.y:.2f} kVm'
+        },
+        
+
+        legend: {
+            enabled: false
+        },
+
+        series: [{
+            name:'Experimental Data',
+            data: graphdata,
+            type: 'scatter'
+        },{
+            name:'Calculated Data',
+            data: graphdata2,
+            type: 'scatter'   
+        }]
+
+    });
+});
+}
