b025dce497f32c46a24451f6fb2ccb5e57f1b54e
Lasse-Fisker <201205998@iha.dk>
2015-11-02 17:05:53 +0100

    Changed startup page
    changed startup page to view data since index hangs and is unneeded

--- SPDS/SPDS/App_Start/RouteConfig.cs
+++ SPDS/SPDS/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace SPDS
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Data", action = "View_Data", id = UrlParameter.Optional }
             );
         }
     }
