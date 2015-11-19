c2ebf6f2ae808b3106fcf531e0bfc87d12756997
David <entvex@gmail.com>
2015-11-19 21:51:52 +0100

    I Administrator.cshtml er stylen blevet flyttet ud i Administrator.css og alt det javascript som kunne er også blivet flyttet ud i Administrator.js. Dog er der stadig noget javascript kode inline i html filen som bruges til at tegne tabel før siden for lov at load helt færdig.
    Administrator.cshtml lister nu brugerne som er hentet fra databasen og bruger http post til at lave actions på dem.
    
    ManageController.cs Der blivet langt ind så den kan håndtere actions.

--- SPDS/SPDS/Controllers/ManageController.cs
+++ SPDS/SPDS/Controllers/ManageController.cs
@@ -1,8 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Web;
+using System.Web.Http.Results;
 using System.Web.Mvc;
+using MSSQLModel;
+using SPDS.Models;
+using SPDS.Models.DbModels;
 
 namespace SPDS.Controllers
 {
@@ -11,7 +17,67 @@ namespace SPDS.Controllers
         // GET: Manage
         public ActionResult Administrator()
         {
+            IDalUserManagement daluserManagement = new MSSQLModelDAL();
+            ViewBag.users = daluserManagement.GetUsers( new ParametersForUsers() );
+
             return View();
         }
+
+        /// <summary>
+        /// Handles the action requests from the Administrator page.
+        /// </summary>
+        /// <param name="email"> user email </param>
+        /// <returns>200 for ok else 404</returns>
+        [HttpPost]
+        public ActionResult Administrator(string email)
+        {
+            if (!String.IsNullOrWhiteSpace(email) && email.Contains(";") && email.Contains("@"))
+            {
+                var mail = email.Split(';');
+
+                IDalUserManagement daluserManagement = new MSSQLModelDAL();
+                var user = daluserManagement.GetUsers(new ParametersForUsers() { Email = mail[1] });
+
+                //If no user was found in the database then return an error
+                if (user[0] == null) return new HttpStatusCodeResult(HttpStatusCode.NotFound);
+
+
+                if (mail[0] == "delete")
+                {
+                    if (mail[1].Contains("@"))
+                    {
+                        daluserManagement.DeleteUser(user.First());
+
+                        return new HttpStatusCodeResult(HttpStatusCode.OK);
+                    }
+                }
+
+                if (mail[0] == "promote")
+                {
+                    if (mail[1].Contains("@"))
+                    {
+                        var perm = daluserManagement.GetPermByAccessLevel(AccessLevel.Reviewer);
+
+                        daluserManagement.UpdateUserPermission(user.First(), perm);
+
+                        return new HttpStatusCodeResult(HttpStatusCode.OK);
+                    }
+                }
+
+                if (mail[0] == "demote")
+                {
+                    if (mail[1].Contains("@"))
+                    {
+                        var perm = daluserManagement.GetPermByAccessLevel(AccessLevel.Submitter);
+
+                        daluserManagement.UpdateUserPermission(user.First(), perm);
+
+                        return new HttpStatusCodeResult(HttpStatusCode.OK);
+                    }
+                }
+            }
+            return new HttpStatusCodeResult(HttpStatusCode.NotFound);
+        }
+
     }
 }
---
+++ SPDS/SPDS/Scripts/Administrator.js
@@ -0,0 +1,81 @@
+﻿function DeleteRowFunction(btndel) {
+    var result = window.confirm("Are you sure?");
+    if (typeof (btndel) == "object" && result == true) {
+
+        var email = $(btndel).closest("tr").find('td:eq(2)').text();
+        console.log("Email:" + email);
+        var request = new XMLHttpRequest();
+        request.open("POST", "Administrator", true);
+        request.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+
+        request.addEventListener("load", function (evt) {
+            if (request.status != 200) {
+                alert("Your change was not made because of an error, you will now be sent to the landing page");
+                window.location.replace(window.location.host);
+            }
+        });
+
+        request.send('email=' + 'delete;' + email);
+
+        $(btndel).closest("tr").remove();
+
+    } else {
+        return false;
+    }
+}
+
+function Promote(btndel) {
+
+    var result = window.confirm("Are you sure?");
+
+    if (typeof (btndel) == "object" && result == true) {
+
+        var email = $(btndel).closest("tr").find('td:eq(2)').text();
+        console.log("Email:" + email);
+        var request = new XMLHttpRequest();
+        request.open("POST", "Administrator", true);
+        request.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+
+        request.addEventListener("load", function (evt) {
+            if (request.status != 200) {
+                alert("Your change was not made because of an error, you will now be sent to the landing page");
+                window.location.replace(window.location.host);
+            }
+        });
+
+        request.send('email=' + 'promote;' + email);
+
+        $(btndel).closest("tr").find('td:eq(3)').text("Reviewer");
+
+    } else {
+        return false;
+    }
+}
+
+function Demote(btndel) {
+
+    var result = window.confirm("Are you sure?");
+
+    if (typeof (btndel) == "object" && result == true) {
+
+        var email = $(btndel).closest("tr").find('td:eq(2)').text();
+        console.log("Email:" + email);
+        var request = new XMLHttpRequest();
+        request.open("POST", "Administrator", true);
+        request.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+
+        request.addEventListener("load", function (evt) {
+            if (request.status != 200) {
+                alert("Your change was not made because of an error, you will now be sent to the landing page");
+                window.location.replace(window.location.host);
+            }
+        });
+
+        request.send('email=' + 'demote;' + email);
+
+        $(btndel).closest("tr").find('td:eq(3)').text("Submitter");
+
+    } else {
+        return false;
+    }
+}
