7f2fb89a333ed076435d9101656fbdb9fb31216d
wartraz <kr.soegaard@gmail.com>
2015-11-19 09:46:27 +0100

    Reset master branch

--- SPDS/SPDS/Scripts/highchart.js
+++ SPDS/SPDS/Scripts/highchart.js
@@ -1,119 +1,120 @@
-function graph( ){
+
+function graph() {
 
 }
 
-graph.prototype.drawGraph = function(graphdata, containername){
-$(function () {
-    $(containername).highcharts({
-
-        chart: {
-            width: 900,
-            height: 600,
-            spacingRight: 20
-        },
-        title: {
-            text: 'Atom# -> Atom#'
-        },
-        subtitle: {
-            text: '#Comment or something here#'
-        },
-
-        xAxis: {
-            type: 'logarithmic',
-            min: 1,
-            tickInterval: 1,
-            minorTickInterval: 0.1,
-            gridLineWidth: 1,
-             title: {
-                text: 'Length (cm)'
-            }
-        },
-
-        yAxis: {
-            type: 'logarithmic',
-            min: 1,
-            tickInterval: 1,
-            minorTickInterval: 0.1,
+graph.prototype.drawGraph = function (graphdata, containername, title, comment) {
+    var chart = new Highcharts.Chart({
+              chart: {
+                  width: 800,
+                  height: 450,
+                  spacingRight: 20,
+                  plotBackgroundColor: null,
+                renderTo: containername
+            },
             title: {
-                text: 'Power (kVm)'
-            }
-        },
-        tooltip: {
-            headerFormat: '<b>{series.name}</b><br>',
-            pointFormat: '{point.x:.2f} cm : {point.y:.2f} kVm'
-        },
-        
+                text: title
+            },
+            subtitle: {
+                text: comment
+            },
 
-        legend: {
-            enabled: false
-        },
+            xAxis: {
+                type: 'logarithmic',
+                min: 1,
+                tickInterval: 1,
+                minorTickInterval: 0.1,
+                gridLineWidth: 1,
+                title: {
+                    text: 'Length (cm)'
+                }
+            },
 
-        series: [{
-            name:'Experimental Data',
-            data: graphdata,
-            type: 'scatter'
-        }]
+            yAxis: {
+                type: 'logarithmic',
+                min: 1,
+                tickInterval: 1,
+                minorTickInterval: 0.1,
+                gridLineWidth: 1,
+                title: {
+                    text: 'Power (kVm)'
+                }
+            },
+            tooltip: {
+                headerFormat: '<b>{series.name}</b><br>',
+                pointFormat: '{point.x:.2f} cm : {point.y:.2f} kVm'
+            },
 
-    });
-});
+
+            legend: {
+                enabled: false
+            },
+
+            series: [{
+                name: 'Experimental Data',
+                data: graphdata,
+                type: 'scatter'
+            }]
+
+        });
 }
 
-graph.prototype.drawGraphDoublePlots = function(graphdata,graphdata2, containername){
-$(function () {
-    $(containername).highcharts({
+graph.prototype.drawGraphDoublePlots = function (graphdata, graphdata2, containername, title, comment) {
+    var chart = new Highcharts.Chart({
 
-        chart: {
-            width: 900,
-            height: 600,
-            spacingRight: 20
-        },
-        title: {
-            text: 'Atom# -> Atom#'
-        },
-        subtitle: {
-            text: '#Comment or something here#'
-        },
-
-        xAxis: {
-            type: 'logarithmic',
-            min: 1,
-            tickInterval: 1,
-            minorTickInterval: 0.1,
-            gridLineWidth: 1,
-             title: {
-                text: 'Length (cm)'
-            }
-        },
-
-        yAxis: {
-            type: 'logarithmic',
-            min: 1,
-            tickInterval: 1,
-            minorTickInterval: 0.1,
+            chart: {
+                width: 800,
+                height: 450,
+                spacingRight: 20,
+                renderTo: containername
+            },
             title: {
-                text: 'Power (kVm)'
-            }
-        },
-        tooltip: {
-            headerFormat: '<b>{series.name}</b><br>',
-            pointFormat: '{point.x:.2f} cm : {point.y:.2f} kVm'
-        },
-        
+                text: title
+            },
+            subtitle: {
+                text: comment
+            },
 
-        legend: {
-            enabled: false
-        },
+            xAxis: {
+                type: 'logarithmic',
+                min: 1,
+                tickInterval: 1,
+                minorTickInterval: 0.1,
+                gridLineWidth: 1,
+                title: {
+                    text: 'Length (cm)'
+                }
+            },
 
-        series: [{
-            name:'Experimental Data',
-            data: graphdata,
-            type: 'scatter'
-        },{
-            name:'Calculated Data',
-            data: graphdata2,
-            type: 'scatter'   
-        }]
+            yAxis: {
+                type: 'logarithmic',
+                min: 1,
+                tickInterval: 1,
+                minorTickInterval: 0.1,
+                title: {
+                    text: 'Power (kVm)'
+                }
+            },
+            tooltip: {
+                headerFormat: '<b>{series.name}</b><br>',
+                pointFormat: '{point.x:.2f} cm : {point.y:.2f} kVm'
+            },
 
-    });
-});
+
+            legend: {
+                enabled: false
+            },
+
+            series: [{
+                name: 'Experimental Data',
+                data: graphdata,
+                type: 'scatter'
+            }, {
+                name: 'Calculated Data',
+                data: graphdata2,
+                type: 'scatter'
+            }]
+
+        });
+
 }
