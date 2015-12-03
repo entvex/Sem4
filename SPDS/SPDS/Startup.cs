afbbe98ed2be02ae4cfc036249d9b5356ead40e1
kaspertorp <kaspertorp@yahoo.dk>
2015-12-03 15:47:13 +0100

    Fixed missing files from View Folder, now published to pauldb-web01.uni.au.dk/

---
+++ SPDS/SPDS/Startup.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.Owin;
+using Owin;
+using Microsoft.AspNet.SignalR;
+
+
+namespace SPDS
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
